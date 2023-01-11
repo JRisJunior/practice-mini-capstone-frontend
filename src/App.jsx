70f83681cf28c4ba8357bac07f47b9d51772bf6e
JRisJunior <jeffmrubin@gmail.com>
2023-01-11 11:49:56 -0600

    add header, footer, and hardcode data

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,100 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <header>
+      <a href="#">Home</a> | <a href="#products-index">All products</a> | <a href="#products-new">New product</a>
+    </header>
+  );
 }
 
-export default App
+function ProductsNew() {
+  return (
+    <div id="products-new">
+      <h1>New recipe</h1>
+      <form>
+        <div>
+          Name: <input type="text" />
+        </div>
+        <div>
+          Price: <input type="text" />
+        </div>
+        <div>
+          description: <input type="text" />
+        </div>
+        <div>
+          Inventory: <input type="text" />
+        </div>
+        <button type="submit">Create product</button>
+      </form>
+    </div>
+  );
+}
+
+function ProductsIndex() {
+  const [products, setProdcuts] = useState([
+    {
+      id: 1,
+      name: "___",
+      price: "___",
+      description: "___",
+      inventory: "___",
+    },
+    {
+      id: 2,
+      name: "___",
+      price: "___",
+      description: "___",
+      inventory: "___",
+    },
+    {
+      id: 3,
+      name: "___",
+      price: "___",
+      description: "___",
+      inventory: "___",
+    },
+  ]);
+
+  return (
+    <div id="products-index">
+      <h1>All Products</h1>
+
+      
+
+
+
+    </div>
+  )
+
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2023</p>
+    </footer>
+  );
+}
+
+function Content() {
+  return (
+    <div>
+      <ProductsNew />
+      <ProductsIndex />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
